13af9b7c865c35678096e30d8bc276c82c6325ab
pedromgoulart@hotmail.com <pedromgoulart@hotmail.com>
2025-02-06 11:22:03 -0300

    subindo projeto

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio-25",
+  base: "/portfolio-25/",
   plugins: [react()],
   css: {
     modules: {
